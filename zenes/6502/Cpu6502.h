a153dc5d65fdcee65308636d6e4f6112467ae251
zenen jaimes <slasherxf@gmail.com>
2016-06-02 05:39:01 -0700

    Added a massive amount of ops. Relative, zero and absolute addressing is taken care of. Relative might be buggy still.

--- zenes/6502/Cpu6502.h
+++ zenes/6502/Cpu6502.h
@@ -18,12 +18,13 @@
 
 @property uint16_t op1;
 @property uint16_t op2;
+@property uint16_t op3;
 @property uint8_t interruptPeriod;
 @property uint8_t counter;
 @property uint8_t reg_acc;
 @property uint8_t reg_x;
 @property uint8_t reg_y;
-@property uint16_t reg_sp;
+@property uint8_t reg_sp;
 @property uint8_t reg_status;
 @property uint16_t reg_pc;
 @property (assign, nonatomic) uint8_t *memory;
@@ -45,10 +46,23 @@
 enum opcodes {
     BPL = 0x10,
     CLD = 0xD8,
+    CMP_IMM = 0xC9,
+    JSR = 0x20,
     LDA_ABS = 0xAD,
+    LDA_ABSX = 0xBD,
+    LDA_ABSY = 0xB9,
     LDA_IMM = 0xA9,
     LDX_IMM = 0xA2,
     LDY_IMM = 0xA0,
+    NOP = 0xEA,
+    ORA_IMM = 0x09,
+    ORA_ZP = 0x05,
+    ORA_ZPX = 0x15,
+    ORA_ABS = 0x0D,
+    ORA_ABSX = 0x1D,
+    ORA_ABSY = 0x19,
+    ORA_IX = 0x01,
+    ORA_IY = 0x11,
     SEI = 0x78,
     STA = 0x8D,
     TXS = 0x9A
--- zenes/6502/Cpu6502.m
+++ zenes/6502/Cpu6502.m
@@ -20,15 +20,15 @@
 
 - (void)bootupSequence {
     // Reset/Boot
-    self.op1 = 0x0;
-    self.op2 = 0x0;
+    self.op1 = 0x00;
+    self.op2 = 0x00;
     self.counter = self.interruptPeriod;
-    self.reg_pc = 0x0;
-    self.reg_status = 0x0;
-    self.reg_x = 0x0;
-    self.reg_y = 0x0;
-    self.reg_acc = 0x0;
-    self.reg_sp = 0x1FFF;
+    self.reg_pc = 0x0000;
+    self.reg_status = 0x00;
+    self.reg_x = 0x00;
+    self.reg_y = 0x00;
+    self.reg_acc = 0x00;
+    self.reg_sp = 0x00;
     uint8_t tempMemory[0x10000] = {};
     
     //TODO: Set everything to 0xFF on bootup. this could be wrong
@@ -99,6 +99,56 @@
     return (self.reg_status & (1 << flag));
 }
 
+- (uint8_t)readZeroPage: (uint8_t)address {
+    if (address > 0xFF) {
+        @throw [NSException exceptionWithName: @"InvalidZeroPage" reason: @"Address passed is great than 0xFF" userInfo: nil];
+    }
+    uint8_t value = self.memory[address];
+    
+    return value;
+}
+
+// Address format is: (address2|address1)+offset
+- (uint8_t)readAbsoluteAddress1: (uint8_t)address1 Address2: (uint8_t)address2 WithOffset: (uint8_t)offset {
+    uint32_t absoluteAddress = 0x10009;//((address2 << 8) | address1)+offset;
+    
+    // Wrap around occurred, go back to the beginning page
+    if (absoluteAddress > 0xFFFF) {
+        absoluteAddress &= 0xFFFF;
+        self.counter--;
+    }
+    
+    NSLog(@"absolute address for %X, %X, %X: %X", (address2 << 8), address1, offset, absoluteAddress);
+    NSLog(@"address: %X value: %X", absoluteAddress, self.memory[absoluteAddress]);
+    return self.memory[absoluteAddress];
+}
+
+- (void)toggleZeroAndSignFlagForReg: (uint8_t)cpu_reg {
+    // CPU Reg is 0, enable the zero flag
+    if (cpu_reg == 0x00) {
+        NSLog(@"Enable Zero Flag");
+        [self enableZeroFlag];
+    } else {
+        NSLog(@"Disable Zero Flag");
+        [self disableZeroFlag];
+    }
+    
+    // Sign flag set on CPU Reg
+    if ((cpu_reg >> 7) & 1) {
+        NSLog(@"Enable Sign");
+        [self enableSignFlag];
+    } else {
+        NSLog(@"Disable Sign");
+        [self disableSignFlag];
+    }
+}
+
+- (void)pushToStack: (uint8_t)data {
+    // Wraps around if need be. reg_sp will be lowered by 1
+    self.memory[0x100+self.reg_sp] = data;
+    self.reg_sp--;
+}
+
 - (void)runNextInstruction {
     enum opcodes opcode;
     opcode = self.memory[self.reg_pc];
@@ -106,27 +156,33 @@
     // setup op1 and op2 even if they aren't used by the operation
     self.op1 = self.reg_pc+1;
     self.op2 = self.reg_pc+2;
-    
-    uint8_t relativeAddress = 0x0;
+    self.op3 = self.reg_pc+3;
     
     NSLog(@"next instruction: %X", opcode);
     NSLog(@"current pc: %X", self.reg_pc);
     
     switch(opcode) {
+        //TODO: Re check the logic on the BPL, rewrite the relative addressing
         // Branch to PC+op1 if negative flag is 0
         case BPL:
             // Branch if the negative bit is set
             if ([self checkFlag: STATUS_NEGATIVE_BIT] == 0) {
                 self.counter -= 3;
-                relativeAddress = self.memory[self.reg_pc+1];
+                uint16_t relativeAddress = self.memory[self.op1];
                 
-                if (relativeAddress > 0x80) {
-                    relativeAddress += (self.reg_pc+1)-256;
-                    relativeAddress &= 0xFFFF;                    
+                if (relativeAddress >= 0x80) {
+                    relativeAddress += (self.reg_pc+4)-256;
+                    relativeAddress &= 0xFFFF;
+                    
+                    NSLog(@"relative address: %X", relativeAddress);
+                    
+                    self.reg_pc = relativeAddress;
+                
+                    // wrap occurred, another cycle occurs
+                    self.counter -= 1;
+                } else {
+                    self.reg_pc += relativeAddress;
                 }
-                NSLog(@"Relative Address: %X", relativeAddress);
-                
-                self.reg_pc += relativeAddress;
                 // TODO: Check for different page here. If extra page used, make sure to add one more cycle
             } else {
                 self.reg_pc += 2;
@@ -136,35 +192,55 @@
             break;
         // CLD (Clear Decimal Flag)
         case CLD:
-            NSLog(@"clear decimal");
             [self disableDecimalFlag];
             // Cycles: 2
             self.counter -= 2;
             // 1 byte OP, jump to the next byte address
             self.reg_pc++;
             break;
+        
+        // CMP (Immediate)
+        case CMP_IMM:
+            self.counter -= 2;
+            uint8_t temp = (self.reg_acc - self.memory[self.op1]);
+            NSLog(@"acc: %d", self.reg_acc);
+            NSLog(@"op1 value: %d", self.memory[self.op1]);
+            NSLog(@"%@", temp >= 0x80?@"greater than 0x80":@"not greater than 0x80");
             
+            self.reg_pc += 2;
+            break;
+            
+        case JSR:
+            NSLog(@"high: %X", self.memory[self.op2]);
+            NSLog(@"low: %X", self.memory[self.op1]);
+            self.counter -= 6;
+            
+            // Push new address to the stack and decrement the current PC
+            [self pushToStack: self.memory[self.reg_pc-1]];
+            [self pushToStack: self.memory[self.reg_pc-2]];
+            self.reg_pc = (self.memory[self.op2] << 8 | self.memory[self.op1]);
+            
+            // Cycles 6
+            break;
+        
+        // LDA Absolute X
+        case LDA_ABSX:
+            // Cycles: 4
+            // TODO: Check for page wrap, add one more cycle to the counter
+            self.counter -= 4;
+            self.reg_acc = [self readAbsoluteAddress1: self.memory[self.op1] Address2: self.memory[self.op2] WithOffset: self.memory[self.op3]];
+            [self toggleZeroAndSignFlagForReg: self.reg_acc];
+            self.reg_pc += 3;
+            break;
         // LDA (Load Acc Immediate)
         case LDA_IMM:
             // Cycles: 2
             self.counter -= 2;
             self.reg_acc = self.memory[self.op1];
+            
             // 1 byte OP, jump to the next byte address
-            NSLog(@"lda imm");
-            
             // Accumulator is 0, enable the zero flag
-            if (self.reg_acc == 0x00) {
-                [self enableZeroFlag];
-            } else {
-                [self disableZeroFlag];
-            }
-            
-            // Sign flag is set if bit 7 is set on the accumulator
-            if ((self.reg_acc >> 7) & 1) {
-                [self enableSignFlag];
-            } else {
-                [self disableSignFlag];
-            }
+            [self toggleZeroAndSignFlagForReg: self.reg_acc];
             
             self.reg_pc += 2;
             break;
@@ -177,18 +253,7 @@
             
             // 1 byte OP, jump to the next byte address
             // Accumulator is 0, enable the zero flag
-            if (self.reg_x == 0x00) {
-                [self enableZeroFlag];
-            } else {
-                [self disableZeroFlag];
-            }
-            
-            // Sign flag is set if bit 7 is set on the reg x
-            if ((self.reg_x >> 7) & 1) {
-                [self enableSignFlag];
-            } else {
-                [self disableSignFlag];
-            }
+            [self toggleZeroAndSignFlagForReg: self.reg_x];
             
             self.reg_pc += 2;
             break;
@@ -200,19 +265,7 @@
             self.reg_y = self.memory[self.op1];
             // 1 byte OP, jump to the next byte address
             
-            // Accumulator is 0, enable the zero flag
-            if (self.reg_y == 0x00) {
-                [self enableZeroFlag];
-            } else {
-                [self disableZeroFlag];
-            }
-            
-            // Sign flag is set if bit 7 is set on the reg y
-            if ((self.reg_y >> 7) & 1) {
-                [self enableSignFlag];
-            } else {
-                [self disableSignFlag];
-            }
+            [self toggleZeroAndSignFlagForReg: self.reg_y];
             
             self.reg_pc += 2;
             break;
@@ -225,22 +278,24 @@
             
             // 1 byte OP, jump to the next byte address
             // Accumulator is 0, enable the zero flag
-            if (self.reg_acc == 0x00) {
-                [self enableZeroFlag];
-            } else {
-                [self disableZeroFlag];
-            }
-            
-            // Sign flag is set if bit 7 is set on the accumulator
-            if ((self.reg_acc >> 7) & 1) {
-                [self enableSignFlag];
-            } else {
-                [self disableSignFlag];
-            }
+            [self toggleZeroAndSignFlagForReg: self.reg_acc];
             
             self.reg_pc += 3;
             break;
+        // NOP (no operation, do nothing but decrement the counter and move on)
+        case NOP:
+            self.counter -=1;
+            self.reg_pc++;
+            break;
+        // ORA on zero page address
+        case ORA_ZP:
+            self.reg_acc |= [self readZeroPage: self.memory[self.op1]];
+            // Cycles
+            self.counter -= 3;
+            [self toggleZeroAndSignFlagForReg: self.reg_acc];
             
+            self.reg_pc += 2;
+            break;
         // SEI (Set Interrupt)
         case SEI:
             [self enableInterrupts];
@@ -260,7 +315,7 @@
             break;
         
         case TXS:
-            self.reg_sp = 0x1000+self.reg_x;
+            self.reg_sp = self.reg_x;
             // Cycles: 2
             self.counter -= 2;
             // 1 byte OP, jump to the next byte address
@@ -270,14 +325,16 @@
             
         // Unknown OP
         default:
-            NSLog(@"OP not found: %X", opcode);
+            NSLog(@"Zero byte read: %X", [self readZeroPage: self.memory[self.op1]]);
+            NSLog(@"OP not found: %X, next 3 bytes %X %X %X", opcode, self.memory[self.op1], self.memory[self.op2], self.memory[self.op3]);
             @throw [NSException exceptionWithName: @"Unknown OP" reason: @"Unknown OP" userInfo: nil];
             break;
     }
     
     // reset ops
-    self.op1 = self.op2 = 0x0;
+    self.op1 = self.op2 = self.op3 = 0x0;
     NSLog(@"status flag: %@", [BitHelper intToBinary: self.reg_status]);
+    NSLog(@"ACC state: %d", self.reg_acc);
 }
 
 - (void)run {
